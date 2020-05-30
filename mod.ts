762ff06c2fbb86371379002b3ec2b2b267e44ade
Zhao Wei Liew <zhaoweiliew@gmail.com>
2020-05-30 19:25:10 +0800

    Enable logger and simple CORS

--- mod.ts
+++ mod.ts
@@ -1,4 +1,7 @@
 import { Application } from "https://deno.land/x/abc/mod.ts";
+import { logger } from "https://deno.land/x/abc/middleware/logger.ts";
+import { cors } from "https://deno.land/x/abc/middleware/cors.ts";
+import { HttpMethod } from "https://deno.land/x/abc/constants.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 import { getRandomWord } from "./database/mod.ts";
 
@@ -8,6 +11,12 @@ const { PORT } = config();
 
 const app = new Application();
 app
+  .use(logger())
+  .use(
+    cors({
+      allowMethods: [HttpMethod.Get],
+    }),
+  )
   .get("/hangman/word", (c) => {
     return getRandomWord();
   })
